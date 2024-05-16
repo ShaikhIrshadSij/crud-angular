c44760424368a3133a03c377d921d9f1bbe53c42
Shaikh Irshadjaha Jahangir <SJahangir@liventus.com>
2024-05-16 18:11:39 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ClientComponent } from './client/client.component';
-import { ReactiveFormsModule } from '@angular/forms'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormComponent, ClientComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, FormComponent, ClientComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,33 +1,38 @@
 import { HttpClient, } from '@angular/common/http';
 import { Component, } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-
+  myform = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    age: new FormControl(),
+    hobbies: new FormControl(''),
+    gender: new FormControl(''),
+    city: new FormControl(''),
+  });
   constructor(private http: HttpClient) { }
 
   submit(data: any) {
     data.preventDefault();
-
-    let body = {
-      "firstname": data.firstName,
-    }
-    this.http.post<any>('https://student-api.mycodelibraries.com/api/student/add', body,)
+    console.log(this.myform.value);
+    const formData = JSON.stringify(this.myform.value);
+    this.http.post<any>('https://student-api.mycodelibraries.com/api/student/add', formData)
       .subscribe(response => {
-        console.log('added data');
+        console.log('added');
         this.resetform();
       },
       );
   }
   resetform() {
-    // this.data.resetForm(); 
+    this.myform.reset();
   }
 
 
